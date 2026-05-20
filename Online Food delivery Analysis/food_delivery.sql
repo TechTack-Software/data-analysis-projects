8bd2f5055eaacbf84718930a43a14571240d4060
poornisenthi05-ai <poornisenthi05@gmail.com>
2026-05-20 13:25:23 +0530

    Add files via upload

---
+++ Online Food delivery Analysis/food_delivery.sql
@@ -0,0 +1,7 @@
+use proj_db;
+select*from swiggy;
+select max(Delivery_time) from swiggy;
+select max(Price) from swiggy;
+select min(Price) from swiggy;
+select*from swiggy order by City desc;
+select*from swiggy order by City asc;

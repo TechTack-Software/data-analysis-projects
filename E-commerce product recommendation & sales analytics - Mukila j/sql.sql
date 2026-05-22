665c94204adc8b1f58a615a2de60c1f18cfb2ec9
mukilajayamani-alt <jayamanimukila@gmail.com>
2026-05-22 00:42:22 -0700

    Add files via upload

---
+++ E-commerce product recommendation & sales analytics - Mukila j/sql.sql
@@ -0,0 +1,9 @@
+show databases;
+create database project;
+use project;
+select * from ecommerce;
+select avg(Price) from ecommerce;
+select min(Rating) from ecommerce;
+select max(Rating) from ecommerce;
+select count(Order_ID) from ecommerce;
+select sum(Quantity) from ecommerce;

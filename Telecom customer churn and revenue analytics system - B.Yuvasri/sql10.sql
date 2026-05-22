791c70d25137850706c8cbb9cfb00ac9ee457cc5
yusw24 <byuvasri2804@gmail.com>
2026-05-22 12:57:32 +0530

    Add files via upload

---
+++ Telecom customer churn and revenue analytics system - B.Yuvasri/sql10.sql
@@ -0,0 +1,67 @@
+show databases;
+create  database del;
+use del;
+select * from telecom;
+select avg (Total_Revenue) from telecom;
+select min(Total_Revenue) from telecom;
+select max(Total_Revenue) from telecom;
+select count(Total_Revenue) from telecom;
+select sum(Total_Revenue) from telecom;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
